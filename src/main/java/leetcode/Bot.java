50143efd8dceca24742fc9707eb2395014f694c4
KatieSanderson <katherina.sanderson@gmail.com>
2020-02-02 17:52:03 +0000

    Moved leetcode question map to app startup

--- src/main/java/leetcode/Bot.java
+++ src/main/java/leetcode/Bot.java
@@ -1,18 +1,71 @@
 package leetcode;
 
+import leetcode.api.LeetcodeApiConnector;
+import leetcode.api.model.DifficultyLevel;
+import leetcode.api.model.LeetcodeQuestion;
+import leetcode.api.model.LeetcodeResponse;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Bot {
+
+    private final String token;
+
+    public Bot(String token) {
+        this.token = token;
+    }
+
+    private void run() throws LoginException {
+        Map<DifficultyLevel, List<LeetcodeQuestion>> leetcodeQuestions = getLeetcodeQuestions();
+        new JDABuilder(token)
+                .addEventListeners(new ReadyListener(leetcodeQuestions))
+                .build();
+    }
+
+    private Map<DifficultyLevel, List<LeetcodeQuestion>> getLeetcodeQuestions() {
+        LeetcodeResponse leetcodeResponse = LeetcodeApiConnector.getLeetcodeResponse();
+        return sortQuestionsByDifficulty(leetcodeResponse);
+    }
+
+    private Map<DifficultyLevel, List<LeetcodeQuestion>> sortQuestionsByDifficulty(LeetcodeResponse leetcodeResponse) {
+        Map<DifficultyLevel, List<LeetcodeQuestion>> map = new HashMap<>();
+        map.put(DifficultyLevel.EASY, new ArrayList<>());
+        map.put(DifficultyLevel.MEDIUM, new ArrayList<>());
+        map.put(DifficultyLevel.HARD, new ArrayList<>());
+
+        List<LeetcodeQuestion> questions = leetcodeResponse.getQuestions();
+        for (LeetcodeQuestion question : questions) {
+            DifficultyLevel level;
+            switch (question.getDifficulty().getLevel()) {
+                case 1 :
+                    level = DifficultyLevel.EASY;
+                    break;
+                case 2 :
+                    level = DifficultyLevel.MEDIUM;
+                    break;
+                case 3 :
+                    level = DifficultyLevel.HARD;
+                    break;
+                default:
+                    throw new RuntimeException("Unknown difficulty level [" + question.getDifficulty().getLevel() + "[ in question [" + question.getStat().getTitle() + "]");
+
+            }
+            map.get(level).add(question);
+        }
+        return map;
+    }
 
     public static void main(String[] args) throws LoginException {
         String token = System.getenv("TOKEN");
         if (token == null) {
             throw new RuntimeException("Environment variable \"TOKEN\" must be provided");
         }
-        new JDABuilder(token)
-                .addEventListeners(new ReadyListener())
-                .build();
+        Bot bot = new Bot(token);
+        bot.run();
     }
 }
--- src/main/java/leetcode/ReadyListener.java
+++ src/main/java/leetcode/ReadyListener.java
@@ -1,9 +1,7 @@
 package leetcode;
 
-import leetcode.api.LeetcodeApiConnector;
 import leetcode.api.model.DifficultyLevel;
 import leetcode.api.model.LeetcodeQuestion;
-import leetcode.api.model.LeetcodeResponse;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.GenericEvent;
@@ -15,7 +13,9 @@ import net.dv8tion.jda.api.hooks.EventListener;
 import javax.annotation.Nonnull;
 import java.time.LocalTime;
 import java.time.ZoneOffset;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -30,6 +30,11 @@ public class ReadyListener implements EventListener {
     private static final String GENERAL_CHANNEL = "general";
 
     private final Map<Guild, ScheduledExecutorService> guilds = new HashMap<>();
+    private final Map<DifficultyLevel, List<LeetcodeQuestion>> leetcodeQuestions;
+
+    public ReadyListener(Map<DifficultyLevel, List<LeetcodeQuestion>> leetcodeQuestions) {
+        this.leetcodeQuestions = leetcodeQuestions;
+    }
 
     @Override
     public void onEvent(@Nonnull GenericEvent event) {
@@ -63,13 +68,11 @@ public class ReadyListener implements EventListener {
     private void runForGuild(Guild guild) {
         TextChannel textChannel = guild.getTextChannelsByName(CHANNEL_NAME, true).get(0);
         try {
-            LeetcodeResponse leetcodeResponse = LeetcodeApiConnector.getLeetcodeResponse();
-            Map<DifficultyLevel, List<LeetcodeQuestion>> map = sortQuestionsByDifficulty(leetcodeResponse);
             ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
             if (System.getenv("RUN_SCHEDULED").toLowerCase().equals("true")) {
-                scheduler.scheduleAtFixedRate(new LeetcodeRunnable(textChannel, map), getSecondsToStart(), SECONDS_IN_A_DAY, TimeUnit.SECONDS);
+                scheduler.scheduleAtFixedRate(new LeetcodeRunnable(textChannel, leetcodeQuestions), getSecondsToStart(), SECONDS_IN_A_DAY, TimeUnit.SECONDS);
             } else {
-                scheduler.schedule(new LeetcodeRunnable(textChannel, map), 0, TimeUnit.SECONDS);
+                scheduler.schedule(new LeetcodeRunnable(textChannel, leetcodeQuestions), 0, TimeUnit.SECONDS);
             }
             guilds.put(guild, scheduler);
         } catch (RuntimeException e) {
@@ -87,33 +90,5 @@ public class ReadyListener implements EventListener {
             secondsToStart += SECONDS_IN_A_DAY;
         }
         return secondsToStart;
-    }
-
-    private Map<DifficultyLevel, List<LeetcodeQuestion>> sortQuestionsByDifficulty(LeetcodeResponse leetcodeResponse) {
-        Map<DifficultyLevel, List<LeetcodeQuestion>> map = new HashMap<>();
-        map.put(DifficultyLevel.EASY, new ArrayList<>());
-        map.put(DifficultyLevel.MEDIUM, new ArrayList<>());
-        map.put(DifficultyLevel.HARD, new ArrayList<>());
-
-        List<LeetcodeQuestion> questions = leetcodeResponse.getQuestions();
-        for (LeetcodeQuestion question : questions) {
-            DifficultyLevel level;
-            switch (question.getDifficulty().getLevel()) {
-                case 1 :
-                    level = DifficultyLevel.EASY;
-                    break;
-                case 2 :
-                    level = DifficultyLevel.MEDIUM;
-                    break;
-                case 3 :
-                    level = DifficultyLevel.HARD;
-                    break;
-                default:
-                    throw new RuntimeException("Unknown difficulty level [" + question.getDifficulty().getLevel() + "[ in question [" + question.getStat().getTitle() + "]");
-
-            }
-            map.get(level).add(question);
-        }
-        return map;
     }
 }
